fd9230a6f268b46f1ab22b3ac15d0d4751a6169a
ArpitJJain <ArpitJJain@users.noreply.github.com>
2026-09-18 12:38:33 +0530

    fix(api): provide tenant dependencies to auth interceptor

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 
+import { AuthorizationModule } from '../authorization/authorization.module';
+import { TenantModule } from '../tenant/tenant.module';
 import { AuthContextStore } from './auth-context.store';
 import { AuthenticationContextInterceptor } from './authentication-context.interceptor';
 import { AuthenticationGuard } from './authentication.guard';
@@ -7,6 +9,7 @@ import { AuthController } from './auth.controller';
 import { JwtAuthenticationService } from './jwt-authentication.service';
 
 @Module({
+  imports: [AuthorizationModule, TenantModule],
   controllers: [AuthController],
   providers: [
     AuthContextStore,
